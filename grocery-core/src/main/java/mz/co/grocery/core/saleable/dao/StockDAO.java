034ff67642b0c5437a0141b4be8ab72c1106c3f7
steliomo <steliom@gmail.com>
2021-03-30 23:24:25 +0200

    GRI-05: Stock query fix

--- grocery-core/src/main/java/mz/co/grocery/core/saleable/dao/StockDAO.java
+++ grocery-core/src/main/java/mz/co/grocery/core/saleable/dao/StockDAO.java
@@ -25,7 +25,7 @@ public interface StockDAO extends GenericDAO<Stock, Long> {
 
 		public static final String fetchByUuid = "SELECT s FROM Stock s INNER JOIN FETCH s.grocery INNER JOIN FETCH s.productDescription pd INNER JOIN FETCH pd.product INNER JOIN FETCH pd.productUnit WHERE s.uuid = :stockUuid AND s.entityStatus = :entityStatus";
 
-		public static final String fetchByProductDescription = "SELECT s FROM Stock s INNER JOIN FETCH s.grocery INNER JOIN FETCH s.productDescription pd INNER JOIN FETCH pd.product INNER JOIN FETCH pd.productUnit WHERE pd.description LIKE :description AND s.entityStatus = :entityStatus";
+		public static final String fetchByProductDescription = "SELECT s FROM Stock s INNER JOIN FETCH s.grocery INNER JOIN FETCH s.productDescription pd INNER JOIN FETCH pd.product p INNER JOIN FETCH pd.productUnit WHERE CONCAT(p.name, ' ', pd.description) LIKE :description AND s.entityStatus = :entityStatus";
 
 		public static final String fetchByGroceryAndProduct = "SELECT s FROM Stock s INNER JOIN FETCH s.grocery g INNER JOIN FETCH s.productDescription pd INNER JOIN FETCH pd.product p INNER JOIN FETCH pd.productUnit WHERE g.uuid = :groceryUuid AND p.uuid = :productUuid AND s.entityStatus = :entityStatus ORDER BY p.name, pd.description";
 
