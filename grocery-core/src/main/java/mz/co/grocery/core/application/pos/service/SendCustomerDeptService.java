f0ddd7113804232347ff0240cedf9c2b85c8cb5f
steliomo <steliom@gmail.com>
2025-11-05 23:24:48 +0200

    Bug Fix for sending whatsApp debt

--- grocery-core/src/main/java/mz/co/grocery/core/application/pos/service/SendCustomerDeptService.java
+++ grocery-core/src/main/java/mz/co/grocery/core/application/pos/service/SendCustomerDeptService.java
@@ -3,14 +3,18 @@
  */
 package mz.co.grocery.core.application.pos.service;
 
+import java.util.List;
+
 import mz.co.grocery.core.application.document.DocumentGeneratorPort;
 import mz.co.grocery.core.application.pos.in.SendCustomerDebtUseCase;
 import mz.co.grocery.core.application.pos.out.SendMessagesPort;
+import mz.co.grocery.core.application.sale.out.SaleItemPort;
 import mz.co.grocery.core.common.Clock;
 import mz.co.grocery.core.common.UseCase;
 import mz.co.grocery.core.domain.customer.Customer;
 import mz.co.grocery.core.domain.document.Document;
 import mz.co.grocery.core.domain.pos.Debt;
+import mz.co.grocery.core.domain.pos.DebtItem;
 import mz.co.grocery.core.domain.pos.DebtReport;
 import mz.co.grocery.core.domain.pos.Message;
 import mz.co.grocery.core.domain.pos.MessageType;
@@ -30,10 +34,15 @@ public class SendCustomerDeptService implements SendCustomerDebtUseCase {
 
 	private SendMessagesPort sendMessagesPort;
 
-	public SendCustomerDeptService(final Clock clock, final DocumentGeneratorPort documentGeneratorPort, final SendMessagesPort sendMessagesPort) {
+	private SaleItemPort saleItemPort;
+
+	public SendCustomerDeptService(final Clock clock, final DocumentGeneratorPort documentGeneratorPort, final SendMessagesPort sendMessagesPort,
+			final SaleItemPort saleItemPort) {
 		this.clock = clock;
 		this.documentGeneratorPort = documentGeneratorPort;
 		this.sendMessagesPort = sendMessagesPort;
+		this.saleItemPort = saleItemPort;
+
 	}
 
 	@Override
@@ -43,14 +52,17 @@ public class SendCustomerDeptService implements SendCustomerDebtUseCase {
 			throw new BusinessException("pos.send.debt.must.have.customer");
 		}
 
-		if (!debt.getDebtItems().isPresent()) {
+		final Customer customer = debt.getCustomer().get();
+		final List<DebtItem> debtItems = this.saleItemPort.findDeptItemsByCustomer(customer.getUuid());
+
+		if (debtItems.isEmpty()) {
 			throw new BusinessException("pos.send.debt.items.cannot.be.empty");
 		}
 
+		debtItems.forEach(debtItem -> debt.addDebtItem(debtItem));
+
 		final Document document = new DebtReport(debt, this.clock);
 		final String filename = this.documentGeneratorPort.generatePdfDocument(document);
-
-		final Customer customer = debt.getCustomer().get();
 
 		final Message textMessage = new Message(customer.getName(),
 				customer.getContact(), MessageType.TEXT,
--- grocery-core/src/test/java/mz/co/grocery/core/pos/SendCustomerDebtUseCaseTest.java
+++ grocery-core/src/test/java/mz/co/grocery/core/pos/SendCustomerDebtUseCaseTest.java
@@ -6,6 +6,8 @@ package mz.co.grocery.core.pos;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 import org.mockito.ArgumentMatchers;
@@ -16,6 +18,7 @@ import org.mockito.Mockito;
 import mz.co.grocery.core.application.document.DocumentGeneratorPort;
 import mz.co.grocery.core.application.pos.out.SendMessagesPort;
 import mz.co.grocery.core.application.pos.service.SendCustomerDeptService;
+import mz.co.grocery.core.application.sale.out.SaleItemPort;
 import mz.co.grocery.core.common.Clock;
 import mz.co.grocery.core.config.AbstractUnitServiceTest;
 import mz.co.grocery.core.domain.customer.Customer;
@@ -43,6 +46,9 @@ public class SendCustomerDebtUseCaseTest extends AbstractUnitServiceTest {
 	@Mock
 	private Clock clock;
 
+	@Mock
+	private SaleItemPort saleItemPort;
+
 	@Test
 	public void shouldSendCustomerDept() throws BusinessException {
 
@@ -50,7 +56,11 @@ public class SendCustomerDebtUseCaseTest extends AbstractUnitServiceTest {
 		debt.setCustomer(EntityFactory.gimme(Customer.class, CustomerTemplate.VALID));
 		debt.addDebtItem(new DebtItem(LocalDate.now(), "Sumo Davita 1L", new BigDecimal(2), new BigDecimal(100), new BigDecimal(200)));
 
+		final List<DebtItem> debtItems = new ArrayList<>();
+		debtItems.add(new DebtItem(LocalDate.now(), "TESTE", new BigDecimal(10), new BigDecimal(5), new BigDecimal(50)));
+
 		Mockito.when(this.clock.todayDateTime()).thenReturn(LocalDateTime.now());
+		Mockito.when(this.saleItemPort.findDeptItemsByCustomer(debt.getCustomer().get().getUuid())).thenReturn(debtItems);
 
 		this.sendCustomerDeptUseCase.sendDebt(debt);
 
