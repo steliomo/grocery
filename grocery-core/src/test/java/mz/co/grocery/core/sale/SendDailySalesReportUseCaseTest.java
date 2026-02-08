4313d00c65ea14eebd6b3456334784b9b4bc7793
steliomo <steliom@gmail.com>
2026-02-08 14:08:29 +0200

    Fix broken tests

--- grocery-core/src/test/java/mz/co/grocery/core/sale/SendDailySalesReportUseCaseTest.java
+++ grocery-core/src/test/java/mz/co/grocery/core/sale/SendDailySalesReportUseCaseTest.java
@@ -3,11 +3,14 @@
  */
 package mz.co.grocery.core.sale;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -20,8 +23,10 @@ import mz.co.grocery.core.application.sale.out.SalePort;
 import mz.co.grocery.core.application.sale.service.SendDailySalesReportService;
 import mz.co.grocery.core.application.unit.out.UnitPort;
 import mz.co.grocery.core.config.AbstractUnitServiceTest;
+import mz.co.grocery.core.domain.sale.SaleReport;
 import mz.co.grocery.core.domain.unit.Unit;
 import mz.co.grocery.core.fixturefactory.UnitTemplate;
+import mz.co.grocery.core.util.ApplicationTranslator;
 import mz.co.msaude.boot.frameworks.exception.BusinessException;
 import mz.co.msaude.boot.frameworks.fixturefactory.EntityFactory;
 
@@ -52,12 +57,18 @@ public class SendDailySalesReportUseCaseTest extends AbstractUnitServiceTest {
 	@Mock
 	private SalePaymentPort salePaymentPort;
 
+	@Mock
+	private ApplicationTranslator translator;
+
 	@Test
 	public void shouldSendDailySalesReport() throws BusinessException {
 
 		final LocalDate saleDate = LocalDate.now();
 
 		Mockito.when(this.unitPort.findUnitsWithDailySales(saleDate)).thenReturn(EntityFactory.gimme(Unit.class, 5, UnitTemplate.VALID));
+
+		Mockito.when(this.salePort.findDailyNumberOfSalesAndTotalSalesByUnit(ArgumentMatchers.any(), ArgumentMatchers.any()))
+		.thenReturn(Optional.of(new SaleReport(LocalDate.now(), 1L, BigDecimal.ONE)));
 
 		final List<Unit> units = this.sendDailySalesReportUseCase.sendReport(saleDate);
 
